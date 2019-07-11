e6ef8ad96f58f596f6f10a38e2e74f8dcdfb3fdd
杨一帆 <18357163209@163.com>
2019-07-11 21:01:16 +0800

    card mapper 增删改查sql代码

---
+++ src/main/java/com/czbank/easylife/mapper/CardMapper.java
@@ -0,0 +1,26 @@
+package com.czbank.easylife.mapper;
+
+import com.czbank.easylife.model.Card;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface CardMapper {
+    @Select("SELECT * FROM card WHERE card_id = #{cardId};")
+    public Card findCardById(@Param("cardId") String cardId);
+
+    @Select("SELECT card.* FROM card a , card_user_map b WHERE a.card_id = b.card_id AND b.user_id = #{userId};")
+    public List<Card> findCardsByUser(@Param("userId") String userId);
+
+    @Insert("INSERT INTO card (card_id,card_psw,card_time,card_loc,card_money,card_type) VALUES (#{cardId},#{cardPsw},#{cardTime},#{cardLoc},#{cardMoney}),#{cardType});")
+    public void addCard(@Param("cardId") String cardId,@Param("cardPsw") String cardPsw,@Param("cardTime") String cardTime,@Param("cardLoc") String cardLoc,@Param("cardMoney") String cardMoney,@Param("cardType") String cardType);
+
+    @Delete("DELETE FROM card WHERE card_id = #{cardId};")
+    public void removeCard(@Param("cardId") String cardId);
+
+    @Update("Update card SET card_money = #{cardMoney} WHERE card_id = #{cardId};")
+    public void uodateCard(@Param("cardId") String cardId);
+}
---
+++ src/main/java/com/czbank/easylife/model/Card.java
@@ -0,0 +1,14 @@
+package com.czbank.easylife.model;
+
+import lombok.Data;
+
+@Data
+public class Card {
+    private String cardId;
+    private String cardPsw;
+    private String cardTime;
+    private String cardLoc;
+    private String cardMoney;
+    private String cardType;
+
+}
