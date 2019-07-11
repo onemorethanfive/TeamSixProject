878923fb6b23861fb89141893bf141c6618e558e
杨一帆 <18357163209@163.com>
2019-07-11 09:53:02 +0800

    spring boot 初始化项目

---
+++ easylife/src/main/java/com/teamsix/easylife/EasylifeApplication.java
@@ -0,0 +1,13 @@
+package com.teamsix.easylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasylifeApplication.class, args);
+    }
+
+}
---
+++ easylife/src/test/java/com/teamsix/easylife/EasylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsix.easylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
