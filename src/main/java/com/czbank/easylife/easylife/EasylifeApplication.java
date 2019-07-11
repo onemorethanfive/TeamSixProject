7b029bd9d7bb66d3a97e6a909eac6812f29fcd3a
杨一帆 <18357163209@163.com>
2019-07-11 15:36:09 +0800

    重新搭建

---
+++ src/main/java/com/czbank/easylife/easylife/EasylifeApplication.java
@@ -0,0 +1,13 @@
+package com.czbank.easylife.easylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasylifeApplication.class, args);
+    }
+
+}
