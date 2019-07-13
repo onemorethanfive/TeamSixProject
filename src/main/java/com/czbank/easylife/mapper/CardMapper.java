980066e96c23455214fc3b24c02bb93762200d62
杨一帆 <18357163209@163.com>
2019-07-13 09:59:57 +0800

    cardmapper bug 修复 int ->unsigned integer

--- src/main/java/com/czbank/easylife/mapper/CardMapper.java
+++ src/main/java/com/czbank/easylife/mapper/CardMapper.java
@@ -15,7 +15,7 @@ public interface CardMapper {
     @Select("SELECT card_money FROM card WHERE card_id = #{cardId};")
     public String getCardMoneyById(@Param("cardId") String cardId);
 
-    @Select("SELECT SUM(CAST(a.card_money AS int)) FROM card a,card_user_map b WHERE a.card_id = b.card_id AND b.user_id =#{userId};")
+    @Select("SELECT SUM(CAST(a.card_money AS  UNSIGNED INTEGER)) FROM card a,card_user_map b WHERE a.card_id = b.card_id AND b.user_id =#{userId};")
     public int getMoneyByUser(@Param("userId") String userId);
 
     @Insert("INSERT INTO card (card_id,card_psw,card_time,card_loc,card_money,card_type) VALUES (#{cardId},#{cardPsw},#{cardTime},#{cardLoc},#{cardMoney}),#{cardType});")
