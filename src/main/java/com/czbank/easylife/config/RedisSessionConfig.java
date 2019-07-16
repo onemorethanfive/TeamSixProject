26319f6004f4706b7a539b3ecf29da0a1f769a0c
杨一帆 <18357163209@163.com>
2019-07-16 19:26:33 +0800

    注释一些内容

--- src/main/java/com/czbank/easylife/config/RedisSessionConfig.java
+++ src/main/java/com/czbank/easylife/config/RedisSessionConfig.java
@@ -1,9 +1,9 @@
-package com.czbank.easylife.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession
-public class RedisSessionConfig {
-}
+//package com.czbank.easylife.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//
+//@Configuration
+//@EnableRedisHttpSession
+//public class RedisSessionConfig {
+//}
