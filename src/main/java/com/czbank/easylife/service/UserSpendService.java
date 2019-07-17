1e53aa7b70d867b55e421de0fae698f5705b66a1
wen <123@qq.com>
2019-07-18 03:27:57 +0800

    fix

--- src/main/java/com/czbank/easylife/service/UserSpendService.java
+++ src/main/java/com/czbank/easylife/service/UserSpendService.java
@@ -21,34 +21,31 @@ public class UserSpendService {
 //list:第一项今日剩余限额，第二项总剩余余额,第三项今日总可用余额
     //输入：userid，今天日期，限额到今天天数，限额总天数，限额金额
     public Map<String,Object> getLimitReminder(String userId, String today ) throws ParseException {
-        List<Bill> todaybill  = billMapper.findBillsByDateUser(today,userId);
-        UserSpend restTillYesterday = userSpendMapper.findTotalspendByIdAndDate(userId,today);
-        Double totalday = Double.valueOf(restTillYesterday.getDatetotal());
-        Double totallimit = Double.valueOf(restTillYesterday.getLimittotal());
+        List<Bill> bills  = billMapper.findBillsByDateUser(today,userId);
+        UserSpend userSpend = userSpendMapper.findTotalspendByIdAndDate(userId,today);
+        Double totalday = Double.valueOf(userSpend.getDatetotal());
+        Double totallimit = Double.valueOf(userSpend.getLimittotal());
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-        Date a = sdf.parse(restTillYesterday.getDate());
-        Date b = sdf.parse(restTillYesterday.getStart());
-        double pastday = a.getTime()-b.getTime();
-        pastday = pastday/(24*60*60*1000);
-        List<Double> answer = new ArrayList<>();
-        double totalamoounttoday = (totallimit-Double.valueOf(restTillYesterday.getTotalspend()))/(totalday-pastday);
-        List<Double> billintoday = new ArrayList<>();
+        Date yesterday = sdf.parse(userSpend.getDate());
+        Date startDay = sdf.parse(userSpend.getStart());
+        double durationDays = yesterday.getTime()-startDay.getTime();
+        durationDays = durationDays/(24*60*60*1000);
+
+        double limitToday = (totallimit-Double.valueOf(userSpend.getTotalspend()))/(totalday-durationDays);
         double sum = 0;
-        for (int i=0;i<todaybill.size();i++){
-            double t=Double.valueOf(todaybill.get(i).getBillNum())*Math.pow(-1,Double.valueOf(todaybill.get(i).getBillType()));
+        for (int i=0;i<bills.size();i++){
+            double t=Double.valueOf(bills.get(i).getBillNum())*Integer.valueOf(bills.get(i).getBillType());
             sum = sum +t;
         }
-        double resttoday=totalamoounttoday-sum;
-        answer.add(resttoday);
-        answer.add(totallimit-sum-Double.valueOf(restTillYesterday.getTotalspend()));
-        answer.add(totalamoounttoday);
+        double restToday=limitToday-sum;
+
         Map<String,Object> result = new HashMap<>();
-        result.put("limitToday",String.valueOf(resttoday));
+        result.put("restToday",String.valueOf(restToday));
         result.put("limitTotal",String.valueOf(totallimit));
-        result.put("restTotal",String.valueOf(totallimit-sum-Double.valueOf(restTillYesterday.getTotalspend())));
-        result.put("restToday",String.valueOf(totalamoounttoday));
-        result.put("startDate",b.toString());
-        result.put("pastDays",pastday);
+        result.put("restTotal",String.valueOf(totallimit-sum-Double.valueOf(userSpend.getTotalspend())));
+        result.put("limitToday",String.valueOf(limitToday));
+        result.put("startDate",sdf.format(startDay));
+        result.put("pastDays",durationDays);
         result.put("totalDay",totalday);
         return result;
     }
